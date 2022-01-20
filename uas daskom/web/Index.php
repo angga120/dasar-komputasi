5e6b690fdd0816c31225b3708e1d50f1b5d6c35e
angga120 <91958642+angga120@users.noreply.github.com>
2022-01-20 09:02:30 +0700

    Add files via upload

---
+++ uas daskom/web/Index.php
@@ -0,0 +1,22 @@
+<?php
+require 'config.php';
+if(!empty($_SESSION["id"])){
+  $id = $_SESSION["id"];
+  $result = mysqli_query($conn, "SELECT * FROM tb_user WHERE id = $id");
+  $row = mysqli_fetch_assoc($result);
+}
+else{
+  header("Location: tugas akhir.html");
+}
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Index</title>
+  </head>
+  <body>
+    <h1>klik logout untuk keluar</h1>
+    <a href="logout.php">Logout</a>
+  </body>
+</html>
